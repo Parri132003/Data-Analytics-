b7c46e98a32d3e3df95cb7fd2feed44fad2244e6
parri132003 <madhuparri25@gmail.com>
2026-09-09 17:31:17 +0530

    Modules

---
+++ PYTHON DAY 20.py
@@ -0,0 +1,78 @@
+'''
+DAY 20(5/09/26)
+
+MODULES:
+a module is a python file(.py) that written using functions,variables,operations,etc....
+eg:-
+import match
+print(math.pow(2,3))
+-------------------------------------------------------------------------------------------------------
+---> BUILT-IN MODULES:
+the modules are developed by programmer and those comes with installation.
+eg:-
+1. math:
+eg:-
+import match
+print(math.pow(2,3))
+
+2. os:
+eg:-
+ import os
+ print(os.getcwd())
+ 
+3.sys:
+eg:-
+import sys
+print(sys.path)
+print(sys.version)
+print(sys.path)
+ 
+4.ranam:
+eg:-
+import random
+print(random.randint(1000,9999))
+
+      
+---> USER-DEFINED MODULES:
+importing specific function from the module
+eg:-
+#create a new file name example:new file is python_new
+from python_new import add_
+print (add_(90,7))
+
+
+--->USING ALIAS NAME:-
+-->calling with the nick name
+syntax:import module as alias name
+eg:
+import python_new as python
+print(python.add_(90,7))
+
+
+
+
+
+
+'''
+
+import python_new as *
+print(e.add_(90,7))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
