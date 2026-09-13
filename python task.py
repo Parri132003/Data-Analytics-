c0846f7a5169d429366527b98ac147e6d4fedfe9
parri132003 <madhuparri25@gmail.com>
2026-09-13 17:31:56 +0530

    codegnan portal task

---
+++ python task.py
@@ -0,0 +1,80 @@
+'''
+                               TASK
+                             ----------
+CREATE A PROGRAM BY TAKING CODEGNAN PORTAL AS EXAMPLE (USING :-LIST,SET,DICTIONARY).
+-----------------------------------------------------------------------------------------
+  codegnan = {
+    "Exam Schedule": ["Monday", "Wednesday", "Friday"],
+
+    "Interview Results": {   
+     "Technical Round": 7,
+        "HR Round": 6
+    },
+
+    "Learning Skills": [
+        "Python",
+        "MSQL",
+        "Excel",
+        "POWER BI",
+        "Data Analytics"
+    ],
+
+    "Completed Projects": {
+        "STUDENT PERFORMANCE SYSTEM",
+        "Sales Analysis",
+        "Customer  Analysis"
+    },
+
+    "Progress": 55.5,
+
+    "Practice Days": 24,
+
+    "Projects Completed": 5
+}
+
+print(codegnan["Exam Schedule"])
+print(codegnan["Interview Results"])
+print(codegnan["Learning Skills"])
+print(codegnan["Completed Projects"])
+print(codegnan["Progress"])
+print(codegnan["Practice Days"])
+print(codegnan["Projects Completed"])
+'''
+codegnan = {
+    "Exam Schedule": ["Monday", "Wednesday", "Friday"],
+
+    "Interview Results": {   
+     "Technical Round": 7,
+        "HR Round": 6
+    },
+
+    "Learning Skills": [
+        "Python",
+        "MSQL",
+        "Excel",
+        "POWER BI",
+        "Data Analytics"
+    ],
+
+    "Completed Projects": {
+        "STUDENT PERFORMANCE SYSTEM",
+        "Sales Analysis",
+        "Customer  Analysis"
+    },
+
+    "Progress": 55.5,
+
+    "Practice Days": 24,
+
+    "Projects Completed": 5
+}
+
+print(codegnan["Exam Schedule"])
+print(codegnan["Interview Results"])
+print(codegnan["Learning Skills"])
+print(codegnan["Completed Projects"])
+print(codegnan["Progress"])
+print(codegnan["Practice Days"])
+print(codegnan["Projects Completed"])
+
+
